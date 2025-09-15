ffc21e8b074ce445fdbeccc6ca022fc6f51e6daa
SALAB2-K3\egzamin <tytus>
2025-09-15 14:37:57 +0200

    added base player movement

--- EscapeAndHide/src/app/app.component.ts
+++ EscapeAndHide/src/app/app.component.ts
@@ -1,13 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'EscapeAndHide';
+
+    @ViewChild("screen", { static: false })
+    canvas!: ElementRef;
+
+    ngAfterViewInit() {
+      
+        let ctx = this.canvas.nativeElement.getContext('2d');
+
+        let gameOver: boolean = false;
+
+        let player = { x: 20, y: 20 }
+        let xVelocity: number = 0;
+        let yVelocity: number = 0;
+
+        function checkGameOver() {
+
+        };
+
+        function movePlayer() {
+            player.x += xVelocity,
+            player.y += yVelocity
+        }
+
+        function drawPlayer() {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(player.x, player.y, 20, 10);
+        };
+
+        function changeDirection(event: any) {
+            const keyPressed = event.keyCode;
+
+            const LEFT = 37;
+            const UP = 38;
+            const RIGHT = 39;
+            const DOWN = 40;
+
+            if (keyPressed == LEFT) {
+                xVelocity = -20;
+                yVelocity = 0;
+                movePlayer();
+            }
+            else if (keyPressed == UP) {
+                xVelocity = 0;
+                yVelocity = -10;
+                movePlayer();
+            }
+            else if (keyPressed == RIGHT) {
+                xVelocity = 20;
+                yVelocity = 0;
+                movePlayer();
+            }
+            else if (keyPressed == DOWN) {
+                xVelocity = 0;
+                yVelocity = 10;
+                movePlayer();
+            }
+        };
+        let id = setInterval(() => {
+            if (!gameOver) {
+                window.addEventListener("keydown", changeDirection);
+                checkGameOver();
+                ctx.fillStyle = 'lightgray';
+                ctx.fillRect(0, 0, 400, 400);
+                drawPlayer();
+            }
+            else {
+                var answer = confirm("Game Over!")
+                if (answer) location.reload();
+                clearInterval(id)
+            }
+        }, 1);
+    }
 }
