bf1cd904c43cff4258d4ac39e4e72e2124ccaa69
tmpara <piechowskikrzyszto27@gmail.com>
2025-09-22 15:26:19 +0200

    (basic) inventory exists and does a thing!!!!!!!!!!

--- EscapeAndHide/src/app/inventory/inventory.component.ts
+++ EscapeAndHide/src/app/inventory/inventory.component.ts
@@ -1,12 +1,6 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Input,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
+import { Item } from './item';
 import * as PIXI from 'pixi.js';
-import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-inventory',
   standalone: true,
@@ -14,34 +8,56 @@ import { AppComponent } from '../app.component';
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.css',
 })
-export class InventoryComponent implements AfterViewInit {
-  @ViewChild('pixiContainer', { static: false }) pixiContainer!: ElementRef;
-  @Input() visible = false;
+export class InventoryComponent {
   app!: PIXI.Application;
 
-  ngAfterViewInit(): void {
-    const app = new PIXI.Application();
-    app.init();
-    this.pixiContainer.nativeElement.appendChild(
-      this.app.view as HTMLCanvasElement
-    );
+  items: Item[] = [];
 
-    this.drawInventory();
+  async ngAfterViewInit() {
+    this.app = new PIXI.Application();
+    await this.app.init({
+      width: 800,
+      height: 800,
+      backgroundColor: 0x1099bb,
+    });
+    document.body.append(this.app.canvas);
+    this.display();
   }
 
-  drawInventory() {
-    const inventorySize = 20;
-    const cellSize = 2;
+  display() {
+    this.app.stage.removeChildren();
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i].displayed = false;
+    }
 
-    for (let i = 0; i < inventorySize; i++) {
-      for (let j = 0; j < inventorySize; j++) {
-        const cell = new PIXI.Graphics();
-        cell.lineStyle(2, 'red');
-        cell.beginFill('blue');
-        cell.rect(i * cellSize, j * cellSize, cellSize, cellSize);
-        cell.endFill();
-        this.app.stage.addChild(cell);
+    for (let i = 0; i < this.items.length; i++) {
+      if (!this.items[i].displayed) {
+        this.items[i].displayed = true;
+        const text = new PIXI.Text({
+          text: this.items[i].name,
+          style: {
+            fontFamily: 'Arial',
+            fontSize: 28,
+            wordWrap: true,
+          },
+        });
+        text.anchor.set(0);
+        text.x = 10;
+        text.y = 10 + i * 40;
+        text.eventMode = 'static';
+        text.onclick = () => this.drop(i);
+        this.app.stage.addChild(text);
       }
     }
+  }
+
+  pickUp(itemName: string) {
+    this.items.push(new Item(itemName, false));
+    this.display();
+  }
+  drop(itemIndex: number) {
+    this.items.splice(itemIndex, 1);
+    this.app.stage.removeChildren(itemIndex, itemIndex + 1);
+    this.display();
   }
 }
